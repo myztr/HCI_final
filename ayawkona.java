c34e61ef20231ce8101f83318756fb4a84245939
myztr <159142824+myztr@users.noreply.github.com>
2024-05-03 14:13:53 +0800

    Add files via upload

---
+++ ayawkona.java
@@ -0,0 +1,17 @@
+// Smooth scrolling for anchor links
+$(document).ready(function() {
+    // Add smooth scrolling to all anchor links
+    $("a").on('click', function(event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+            var hash = this.hash;
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function(){
+                window.location.hash = hash;
+            });
+        }
+    });
+});
+
+// Add your custom JavaScript functionality here
